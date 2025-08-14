75081a8ef5fe23dbce6c552a14923a9dfb52afc6
aminehadefdev <amine.hadef.dev@gmail.com>
2025-08-15 00:34:13 +0200

    erreur de commit

--- src/stors/slice.js
+++ src/stors/slice.js
@@ -1,15 +1,22 @@
 import { createSlice } from '@reduxjs/toolkit';
 import Area from "./Area/Area";
 import Canon from './Canon/Canon';
+import Ship from './Ship/Ship';
 
 const initialState = {
     Area: Area.initialState,
-    Canon: Canon.initialState
+    Canon: Canon.initialState,
+    Ship: Ship.initialState,
 };
 const reducers = {
     setDirection: Canon.reducers.setDirection,
     moveCanonRight: Canon.reducers.moveCanonRight,
     moveCanonLeft: Canon.reducers.moveCanonLeft,
+
+    moveShipRight: Ship.reducers.moveShipRight,
+    moveShipLeft: Ship.reducers.moveShipLeft,
+    moveShipBottom: Ship.reducers.moveShipBottom,
+    setShipDirection: Ship.reducers.setShipDirection
 }
 
 const Slice = createSlice({
@@ -22,6 +29,12 @@ export const {
     setDirection,
     moveCanonRight,
     moveCanonLeft,
+
+    moveShipBottom,
+    moveShipLeft,
+    moveShipRight,
+    setShipDirection,
+
 } = Slice.actions;
 
 export default Slice.reducer;
