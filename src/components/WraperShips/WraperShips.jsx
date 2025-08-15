1eee7ab558f1795be2fffa466e67e16857ca8e3a
aminehadefdev <amine.hadef.dev@gmail.com>
2025-08-15 18:26:54 +0200

    ajout des direction et la logique de changement de direction pour le wrapper des ships

--- src/components/WraperShips/WraperShips.jsx
+++ src/components/WraperShips/WraperShips.jsx
@@ -1,26 +1,29 @@
 import { useSelector, useDispatch } from "react-redux"
 import "./WraperShips.css"
 import { useEffect } from "react"
-import { moveWrapperShipRight, moveWrapperShipLeft } from '../../stors/slice';
+import { moveWrapperShipRight, moveWrapperShipLeft, moveWrapperShipBottom } from '../../stors/slice';
 
 //import Ship from "../Ship/Ship"
 
 
 function WraperShips() {
-    const store = useSelector(state => state.App)    
+    const store = useSelector(state => state.App)
     const dispatch = useDispatch()
     useEffect(() => {
         let animationFrame;
         const move = () => {
-            if (store.WraperShips.direction === "right") {
-                dispatch(moveWrapperShipRight());
+            switch (store.WraperShips.direction) {
+                case "right":
+                    dispatch(moveWrapperShipRight());
+                    break;
+                case "left":
+                    dispatch(moveWrapperShipLeft());
+                    break;
+                case 'bottom':
+                    dispatch(moveWrapperShipBottom());
+                    break;
             }
-            if (store.WraperShips.direction === "left") {
-                dispatch(moveWrapperShipLeft());
-            }
-            if (store.WraperShips.direction) {
-                animationFrame = requestAnimationFrame(move);
-            }
+            animationFrame = requestAnimationFrame(move);
         }
         if (store.WraperShips.direction) {
             animationFrame = requestAnimationFrame(move);
@@ -28,11 +31,12 @@ function WraperShips() {
 
         return () => cancelAnimationFrame(animationFrame);
     }, [store.WraperShips.direction, dispatch])
-    
+
     const style = {
         width: store.WraperShips.width,
         height: store.WraperShips.height,
-        left: store.WraperShips.y
+        left: store.WraperShips.y,
+        top: store.WraperShips.x
     }
 
     return (
--- src/stors/WraperShips/WraperShips.js
+++ src/stors/WraperShips/WraperShips.js
@@ -4,30 +4,48 @@ const WraperShips = {
         height: 200,
         x: 0,
         y: 0,
-        direction: "right",
+        direction: "",
         speed: 1,
         diposition: [
             ["A", "A", "A", "A", "A", "A",],
             ["A", "A", "A", "A", "A", "A",],
             ["A", "A", "A", "A", "A", "A",],
-        ]
+        ],
+        counter:0
     },
 
     reducers: {
         moveWrapperShipRight: (state) => {  
             if(state.WraperShips.y < state.Area.width - state.WraperShips.width){
                 state.WraperShips.y = state.WraperShips.y + state.WraperShips.speed
-            }        
+            }else{
+                state.WraperShips.counter = state.WraperShips.counter + 30 
+                state.WraperShips.direction = "bottom"
+            }
         },
         moveWrapperShipLeft: (state) => {
-            state.WraperShips.y = state.WraperShips.y - state.WraperShips.speed
+            if(state.WraperShips.y >= 0){
+                state.WraperShips.y = state.WraperShips.y - state.WraperShips.speed
+            }else{
+                state.WraperShips.counter = state.WraperShips.counter + 30 
+                state.WraperShips.direction = "bottom"
+            }
         },
         moveWrapperShipBottom: (state) => {
-            state.WraperShips.x = state.WraperShips.x - state.WraperShips.speed
+            if (state.WraperShips.x <= state.WraperShips.counter) {
+                state.WraperShips.x = state.WraperShips.x + state.WraperShips.speed
+            }else{
+                if(state.WraperShips.y <= 0){
+                    state.WraperShips.direction = 'right'
+                }else{
+                    state.WraperShips.direction = 'left'
+                }
+            }
         },
         setWrapperShipDirection: (state, action) => {
             state.WraperShips.direction = action.payload.direction
         },
+
     }
 };
 
